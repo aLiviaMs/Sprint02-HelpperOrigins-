97653a880516f60c90e17ec866c02f9d962c8c57
Lívia Minucelli <liviawrk@gmail.com>
2021-11-08 00:17:58 -0300

    .

--- Ex5/VowelCounter.ts
+++
@@ -1,8 +0,0 @@
-export default class VowelCounter {
-    private static Vowels: string[] = ['a', 'e', 'i', 'o', 'u'];
-
-    static count(word: string): number {
-        const totalVowels: string[] = word.toLowerCase().split("").filter((letter: string) => this.Vowels.includes(letter));
-        return totalVowels.length;
-    }
-}
